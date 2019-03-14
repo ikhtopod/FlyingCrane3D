057ae80759966675ef4c84e784467c583b8886ed
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-03-14 08:24:35 +0300

    Mesh edited

--- 3DEditor/Mesh.cpp
+++ 3DEditor/Mesh.cpp
@@ -1,4 +1,67 @@
 #include "Mesh.h"
 
 
-Mesh::Mesh() {}
+const GLsizei Mesh::BUFFER_SIZE = 1;
+
+Mesh::Mesh() {
+	this->vertices = {
+		glm::vec3 { 1.0f, 0.0f, -1.0f },
+		glm::vec3 { 1.0f, 0.0f, 1.0f },
+		glm::vec3 { -1.0f, 0.0f, 1.0f },
+		glm::vec3 { -1.0f, 0.0f, -1.0f },
+		glm::vec3 { 0.0f, 2.0f, 0.0f },
+	};
+
+	this->indices = {
+		1, 3, 0,
+		0, 4, 1,
+		1, 4, 2,
+		2, 4, 3,
+		4, 0, 3,
+		1, 2, 3,
+	};
+}
+
+Mesh::Mesh(std::vector<Vertex> _vertices, std::vector<GLuint> _indices)
+	: vertices(_vertices), indices(_indices) {}
+
+Mesh::~Mesh() {}
+
+
+void Mesh::init() {
+	glGenVertexArrays(BUFFER_SIZE, &this->vao);
+	glGenBuffers(BUFFER_SIZE, &this->vbo);
+	glGenBuffers(BUFFER_SIZE, &this->ebo);
+
+	glBindVertexArray(this->vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, this->vbo);
+	glBufferData(GL_ARRAY_BUFFER, this->vertices.size() * sizeof(Vertex), &(this->vertices)[0], GL_STREAM_DRAW); // GL_STATIC_DRAW, GL_DYNAMIC_DRAW, GL_STREAM_DRAW
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->indices.size() * sizeof(GLuint), &(this->indices)[0], GL_STREAM_DRAW);
+
+	// vertex positions
+	glEnableVertexAttribArray(Mesh::AttribIndex::POSITION);
+	glVertexAttribPointer(Mesh::AttribIndex::POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+
+	// vertex normals
+	//glEnableVertexAttribArray(Mesh::AttribIndex::NORMAL);
+	//glVertexAttribPointer(Mesh::AttribIndex::NORMAL, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+
+	glBindVertexArray(0); // unbind
+}
+
+void Mesh::draw() {
+	glBindVertexArray(this->vao);
+	glDrawElements(GL_TRIANGLES, this->indices.size(), GL_UNSIGNED_INT, (void*)0);
+	glBindVertexArray(0); // unbind
+}
+
+
+void Mesh::free() {
+	glDeleteVertexArrays(BUFFER_SIZE, &this->vao);
+	glDeleteBuffers(BUFFER_SIZE, &this->vbo);
+	glDeleteBuffers(BUFFER_SIZE, &this->ebo);
+}
+
--- 3DEditor/Mesh.h
+++ 3DEditor/Mesh.h
@@ -1,12 +1,35 @@
 #pragma once
 
+#include "Vertex.h"
+#include "Polygon.h"
+
+
 class Mesh {
 private:
-	std::vector<glm::vec3> vertices;
-	std::vector<glm::vec3> normals;
-	std::vector<glm::vec3> faces; // indices class Face
-	//std::vector<glm::vec2> textureCoords;
+	static const GLsizei BUFFER_SIZE;
+
+	enum AttribIndex : GLuint {
+		POSITION,
+		// NORMAL,
+		// TEXTURE,
+		// TANGENT,
+		// BITANGENT,
+	};
+
+private:
+	GLuint vao;
+	GLuint vbo;
+	GLuint ebo;
+
+	std::vector<Vertex> vertices;
+	std::vector<GLuint> indices;
+
 public:
 	Mesh();
-	~Mesh() = default;
+	Mesh(std::vector<Vertex> _vertices, std::vector<GLuint> _indices);
+	~Mesh();
+
+	void init();
+	void draw();
+	void free();
 };
