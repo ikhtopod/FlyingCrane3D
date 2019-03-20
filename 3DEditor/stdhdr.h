10865c8f602c3a46f19e0181ed7a0482d24b0416
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-03-20 22:08:36 +0300

    подключены дополнительные библиотеки glm

--- 3DEditor/stdhdr.h
+++ 3DEditor/stdhdr.h
@@ -14,6 +14,11 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/transform.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
+
 // future include: imgui
 
 #include <iostream>
