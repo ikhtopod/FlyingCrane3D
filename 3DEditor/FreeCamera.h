210edcd63a52fae98c37742330d808a19f235dc7
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-03-22 18:03:23 +0300

    edited

--- 3DEditor/FreeCamera.h
+++ 3DEditor/FreeCamera.h
@@ -4,6 +4,9 @@
 
 
 class FreeCamera : public Camera {
+private:
+	glm::vec2 lastMousePosition {};
+
 public:
 	virtual void keyboardInput() override;
 	virtual void mouseInput(float xPos, float yPos) override;
