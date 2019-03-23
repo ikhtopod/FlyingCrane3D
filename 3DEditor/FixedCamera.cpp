f12fe89e1c52d718b07fb3eb0f8c0cabad3eef11
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-03-23 16:51:40 +0300

    Новая камера, которая действует в обычном режиме, т.е. когда камерой не манипулируют

---
+++ 3DEditor/FixedCamera.cpp
@@ -0,0 +1,10 @@
+#include "FixedCamera.h"
+
+
+void FixedCamera::keyboardInput() {}
+
+void FixedCamera::mouseInput(float xPos, float yPos) {}
+
+void FixedCamera::mouseButtonInput(int button, int action, int mods) {}
+
+void FixedCamera::mouseScrollInput(float xOffset, float yOffset) {}
