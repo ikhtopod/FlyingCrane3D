7af85aa9b017d82843c2a1288ca5ddabe4402210
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-03-25 00:45:58 +0300

    добавлена перегрузка repeat для glm::vec2

--- 3DEditor/Util.h
+++ 3DEditor/Util.h
@@ -11,6 +11,7 @@ public:
 public:
 	static float repeat(float val, float _min, float _max);
 	static glm::vec3 repeat(glm::vec3 val, float _min, float _max);
+	static glm::vec2 repeat(glm::vec2 val, float _min, float _max);
 
 	static std::string getTextFromFile(const std::filesystem::path& filePath);
 
