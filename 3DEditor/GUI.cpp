614c0eb5e8e423a9805cd215fdee9c6844868e3b
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-04-03 23:07:46 +0300

    gui updated

--- 3DEditor/GUI.cpp
+++ 3DEditor/GUI.cpp
@@ -77,6 +77,7 @@ void GUI::draw_Render() {
 
 void GUI::showMainMenuBar() {
 	static bool showAboutWindow = false;
+	static bool showHotKeys = false;
 
 	if (ImGui::BeginMainMenuBar()) {
 		if (ImGui::BeginMenu("Файл")) {
@@ -99,21 +100,60 @@ void GUI::showMainMenuBar() {
 		}
 
 		if (ImGui::BeginMenu("Справка")) {
+			if (ImGui::MenuItem("Горячие клавиши", "", &showHotKeys)) {}
+			
+			if (ImGui::BeginMenu("Статистика")) {
+				ImGui::Text(("FPS: " + std::to_string(ImGui::GetIO().Framerate)).c_str(), "");
+				ImGui::Separator();
+				ImGui::EndMenu();
+			}
+			
+			ImGui::Separator();
 			if (ImGui::MenuItem("О проекте", "F1", &showAboutWindow)) {}
 			ImGui::EndMenu();
 		}
 
+		ImGui::EndMainMenuBar();
+
 		if (showAboutWindow) {
 			ImGui::Begin("О проекте", &showAboutWindow);
 
-			ImGui::Text("Дипломный проект на тему:\n\n");
-			ImGui::Text("\"Разработка программной платформы интерактивной 3D-визуализации\"\n\n");
+			ImGui::Text("Дипломный проект на тему:"); ImGui::NewLine();
+			ImGui::Text("\"Разработка программной платформы интерактивной 3D-визуализации\"");
+			ImGui::NewLine();
 			ImGui::Separator();
 			ImGui::Text("Виталий Лифанов. Группа ЗП3-2д");
 
 			ImGui::End();
 		}
 
-		ImGui::EndMainMenuBar();
+		if (showHotKeys) {
+			ImGui::Begin("Горячие клавиши", &showHotKeys);
+
+			ImGui::Text("Tab: переключить камеру"); ImGui::NewLine();
+			ImGui::Separator();
+			ImGui::Text("Free Camera:"); ImGui::NewLine();
+			ImGui::Text("W, A, S, D, Q, E: передвижение по сцене");
+			ImGui::Text("Mouse: направление движения");
+			ImGui::Text("Крутить СКМ: скорость"); ImGui::NewLine();
+			ImGui::Separator();
+			ImGui::Text("Target Camera:"); ImGui::NewLine();
+			ImGui::Text("Зажать СКМ: вращение вокруг целевой точки");
+			ImGui::Text("Shift + СКМ: смещение камеры и целевой точки в стороны");
+			ImGui::Text("Крутить СКМ: изменение расстояния от целевой точки"); ImGui::NewLine();
+			ImGui::Separator();
+			ImGui::Text("Выделение:"); ImGui::NewLine();
+			ImGui::Text("ЛКМ: выделить объект");
+			ImGui::Text("Shift + ЛКМ: выделить несколько объектов"); ImGui::NewLine();
+			ImGui::Text("Сменить режим выделения:"); ImGui::NewLine();
+			ImGui::Text("1: выделение вершин");
+			ImGui::Text("2: выделение ребер");
+			ImGui::Text("3: выделение граней");
+			ImGui::Text("4: выделение объектов");
+			ImGui::Separator();
+
+			ImGui::End();
+		}
+
 	}
 }
