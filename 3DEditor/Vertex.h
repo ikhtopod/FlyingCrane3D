7553aa26a65d0e7ad8d07c0f2f50e7b2a83f4b7b
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-04-28 15:37:07 +0300

    removed comments

--- 3DEditor/Vertex.h
+++ 3DEditor/Vertex.h
@@ -3,9 +3,6 @@
 struct Vertex {
 	glm::vec3 position;
 	glm::vec3 normal;
-	//glm::vec2 texture;
-	//glm::vec3 tangent;
-	//glm::vec3 bitangent;
 
 	Vertex(glm::vec3 _position);
 	Vertex(glm::vec3 _position, glm::vec3 _normal);
