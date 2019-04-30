ea1390eb10c9cf244c5cbfbd1a1f8a5747c939cd
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-04-30 18:34:56 +0300

    index added to Vertex class

--- 3DEditor/Vertex.h
+++ 3DEditor/Vertex.h
@@ -4,8 +4,10 @@ struct Vertex {
 	glm::vec3 position;
 	glm::vec3 normal;
 
-	Vertex(glm::vec3 _position);
-	Vertex(glm::vec3 _position, glm::vec3 _normal);
+	GLuint index;
+
+	Vertex(glm::vec3 _position, GLuint _index);
+	Vertex(glm::vec3 _position, glm::vec3 _normal, GLuint _index);
 
 	bool operator==(const Vertex& v1) const;
 };
