3f01744ab4ca40d14f1c7b75aa3d5c898ecc6a34
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-05-05 18:34:53 +0300

    Added Mark for MeshElementFace

--- 3DEditor/MeshElementFace.cpp
+++ 3DEditor/MeshElementFace.cpp
@@ -11,4 +11,51 @@ MeshElementFace::MeshElementFace(Vertex& first, Vertex& second, Vertex& third) :
 
 	this->vertices.insert(this->vertices.end(), { first, second, third });
 	this->indices.insert(this->indices.end(), { 0, 1, 2 });
+
+	glm::vec3 markCenter = (first.position + second.position + third.position) / 3.0f;
+	float scale = 0.05f;
+
+	this->markPoints.insert(
+		this->markPoints.end(), {
+			{ ((first.position - markCenter) * scale) + markCenter, 0 },
+			{ ((second.position - markCenter) * scale) + markCenter, 1 },
+			{ ((third.position - markCenter) * scale) + markCenter, 2 },
+		}
+	);
+}
+
+void MeshElementFace::initMark() {
+	glGenVertexArrays(BUFFER_SIZE, &this->vaoMark);
+	glGenBuffers(BUFFER_SIZE, &this->vboMark);
+
+	glBindVertexArray(this->vaoMark);
+
+	glBindBuffer(GL_ARRAY_BUFFER, this->vboMark);
+	glBufferData(GL_ARRAY_BUFFER, this->markPoints.size() * sizeof(Vertex),
+				 &(this->markPoints)[0], GL_STREAM_DRAW);
+
+	// vertex positions
+	glEnableVertexAttribArray(MeshBase::AttribIndex::POSITION);
+	glVertexAttribPointer(MeshBase::AttribIndex::POSITION, 3,
+						  GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+
+	glBindVertexArray(0); // unbind
+}
+
+void MeshElementFace::drawMark() {
+	glBindVertexArray(this->vaoMark);
+	glLineWidth(2.5f);
+	glDrawArrays(GL_LINE_LOOP, 0, 3);
+	glLineWidth(1.0f);
+	glBindVertexArray(0); // unbind
+}
+
+void MeshElementFace::init() {
+	MeshElement::init();
+	this->initMark();
+}
+
+void MeshElementFace::draw() {
+	MeshElement::draw();
+	this->drawMark();
 }
--- 3DEditor/MeshElementFace.h
+++ 3DEditor/MeshElementFace.h
@@ -8,10 +8,20 @@ class MeshElementFace final : public MeshElement {
 public:
 	static const GLenum DEFAULT_MESH_TYPE;
 
+private:
+	std::vector<Vertex> markPoints {};
+	GLuint vaoMark, vboMark;
+
+	void initMark();
+	void drawMark();
+
 public:
 	MeshElementFace() = delete;
 	MeshElementFace(Face& _face);
 	MeshElementFace(Vertex& first, Vertex& second, Vertex& third);
 	virtual ~MeshElementFace() = default;
+
+	virtual void init() override;
+	virtual void draw() override;
 };
 
